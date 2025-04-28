6dbd533eec31551aa83799829efb94f25f7d3cfb
Angela Alvarez <angelamariaalag@gmail.com>
2025-04-28 10:23:56 -0500

    Update App.py

--- App.py
+++ App.py
@@ -30,6 +30,16 @@ page_bg_img = """
     background-image: url("https://static.vecteezy.com/system/resources/previews/008/218/160/non_2x/horizontal-topographic-map-black-topographer-seamless-pattern-dark-typography-linear-background-for-mapping-and-audio-equalizer-backdrop-illustration-vector.jpg");
     background-size: cover;
 }
+
+h1, h2 {
+    text-align: center;
+    color: white;
+}
+
+button {
+    display: block;
+    margin: 0 auto;
+}
 </style>
 """
 st.markdown(page_bg_img, unsafe_allow_html=True)
@@ -46,16 +56,16 @@ with st.sidebar:
     stroke_width = st.slider('Selecciona el ancho de línea', 1, 30, 15, key='stroke_width_slider')
     bg_color = st.color_picker('Selecciona el color de fondo', '#000000', key='bg_color_picker')
 
-# Crear columnas para centrar el canvas
-col1, col2, col3 = st.columns([1, 2, 1])  # Columnas para centrar el canvas
+# Crear columnas para centrar todo
+col1, col2, col3 = st.columns([1, 2, 1])
 
-with col2:  # Canvas en la columna central
+with col2:  # Todo va en la columna central
     canvas_result = st_canvas(
         fill_color="rgba(255, 165, 0, 0.3)",
         stroke_width=stroke_width,
         stroke_color=stroke_color,
         background_color=bg_color,
-        height=300,  # Puedes ajustar a 300 si quieres que sea un poquito más grande
+        height=300,
         width=300,
         drawing_mode="freedraw",
         key="canvas",
@@ -72,3 +82,4 @@ with col2:  # Canvas en la columna central
             st.header('El Dígito es : ' + str(res))
         else:
             st.header('Por favor dibuja en el canvas el dígito.')
+
